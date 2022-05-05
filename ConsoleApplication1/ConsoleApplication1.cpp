6ce9a5498f645171f011129edb4889913a9255a1
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2022-05-05 16:16:42 +0930

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+//it is very important to understand how C++ compilation works and it will help you to understand how programs are complied and executed
+//There are 4 major steps to compile C++ source code into machine-readable code.
+// The first step is we will progress the source code
+// The second step is we will compile source code
+// The third step is we will asseble the compiled file
+// The final step is we will link the object code file to create an executable file
+int main()
+{
+    std::cout << "Hello World!\n";// it will output the result which is Hello world in the console
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
